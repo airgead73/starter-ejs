1a13ff87d5d9567f0ae94c9c8f31cf5e9213afb6
Dan M <moneypenny.dan@gmail.com>
2022-05-14 23:03:16 -0700

    Delete gulp directory.

--- gulpfile.js/production/index.js
+++
@@ -1,6 +0,0 @@
-const { series } = require('gulp');
-const scss = require('./scss');
-const clean = require('./clean');
-const javascript = require('./javascript')
-
-exports.build = series(clean, scss, javascript)
