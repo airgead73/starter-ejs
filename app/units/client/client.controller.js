c3a39755189f3762839ebe0918d2d57f18044549
Dan Moneypenny <catullus1973@gmail.com>
2022-05-15 01:56:53 -0700

    reset views

--- app/units/client/client.controller.js
+++ app/units/client/client.controller.js
@@ -1,5 +1,5 @@
 const asyncHandler = require('express-async-handler');
-const { isDev } = require('../../config/env');
+const { isDev, isPr } = require('../../config/env');
 
 /**
  * @desc dashboard view
@@ -10,84 +10,5 @@ const { isDev } = require('../../config/env');
 exports.dashboard = asyncHandler(async (req, res, next) => {
   return res
     .status(200)
-    .render('pages/dashboard', {
-      success: true,
-      title: 'tracker'
-    });
-});
-
-/**
- * @desc start shift view
- * @route GET - /add
- * @access Private
- * */
-
- exports.start = asyncHandler(async (req, res, next) => {
-  return res
-    .status(200)
-    .render('pages/start', {
-      success: true,
-      title: 'start shift'
-    });
-});
-
-
-/**
- * @desc add items view
- * @route GET - /add
- * @access Private
- * */
-
- exports.add = asyncHandler(async (req, res, next) => {
-  return res
-    .status(200)
-    .render('pages/add', {
-      success: true,
-      title: 'add items'
-    });
-});
-
-/**
- * @desc active view
- * @route GET - /active
- * @access Private
- * */
-
- exports.active = asyncHandler(async (req, res, next) => {
-  return res
-    .status(200)
-    .render('pages/active', {
-      success: true,
-      title: 'active shift'
-    });
-});
-
-/**
- * @desc detail shift view
- * @route GET - /:id
- * @access Private
- * */
-
- exports.detail = asyncHandler(async (req, res, next) => {
-  return res
-    .status(200)
-    .render('pages/detail', {
-      success: true,
-      title: 'update'
-    });
-});
-
-/**
- * @desc update shift view
- * @route GET - /:id/update
- * @access Private
- * */
-
- exports.update = asyncHandler(async (req, res, next) => {
-  return res
-    .status(200)
-    .render('pages/update', {
-      success: true,
-      title: 'update'
-    });
+    .send('dashboard');
 });
--- app/units/client/client.routes.js
+++ app/units/client/client.routes.js
@@ -2,14 +2,9 @@ const { Router } = require('express');
 const clientRouter = Router();
 
 // controllers
-const { dashboard, start, add, active, detail, update } = require('./client.controller');
+const { dashboard } = require('./client.controller');
 
 // routes
 clientRouter.route('/').get(dashboard);
-clientRouter.route('/start').get(start);
-clientRouter.route('/add').get(add);
-clientRouter.route('/active').get(active);
-clientRouter.route('/:id').get(detail);
-clientRouter.route('/:id/update').get(update);
 
 module.exports = clientRouter;
