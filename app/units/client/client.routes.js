9e1bb321402f208e38e25f5223689ae5bc7d0b8d
Dan Moneypenny <catullus1973@gmail.com>
2022-05-16 01:21:08 -0700

    add author views

--- app/units/client/client.routes.js
+++ app/units/client/client.routes.js
@@ -2,12 +2,17 @@ const { Router } = require('express');
 const clientRouter = Router();
 
 // controllers
-const { dashboard } = require('./client.controller');
+const { dashboard, dashboardAuthors, addAuthor, detailAuthor, updateAuthor } = require('./client.controller');
 
 // middleware
 const { checkAuthClient } = require('../../middleware');
+clientRouter.use(checkAuthClient);
 
 // routes
-clientRouter.route('/').get(checkAuthClient, dashboard);
+clientRouter.route('/').get(dashboard);
+clientRouter.route('/authors').get(dashboardAuthors);
+clientRouter.route('/authors/add').get(addAuthor);
+clientRouter.route('/authors/:id').get(detailAuthor);
+clientRouter.route('/authors/:id/update').get(updateAuthor);
 
 module.exports = clientRouter;
