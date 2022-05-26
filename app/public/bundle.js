cc9e00a7498956bbc97797757e46be29d232d6c4
Dan Moneypenny <catullus1973@gmail.com>
2022-05-26 03:18:40 -0700

    build

--- app/public/bundle.js
+++ app/public/bundle.js
@@ -1,3 +1,9 @@
+(function () {
+'use strict';
+
 const jsScripts = Array.from(document.querySelectorAll('[data-script]'));
 
 if(jsScripts.length) console.log(jsScripts);
+
+}());
+//# sourceMappingURL=bundle.js.map
---
+++ assets/scripts/authors/index.js
@@ -0,0 +1,5 @@
+import addAuthor from './add';
+
+export {
+  addAuthor
+}
